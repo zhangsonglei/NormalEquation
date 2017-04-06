86a8e85120f238d155d9df2d463741866e459206
张松磊 <2594454098@qq.com>
2017-04-06 19:42:30 +0800

    complete

--- src/com/hust/sonly/Normal_Equation.java
+++ src/com/hust/sonly/Normal_Equation.java
@@ -25,19 +25,28 @@ public class Normal_Equation {
 	}
 	
 	
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 //		double[][] X = {{2014,5,1,45},{1416,3,2,40},{1514,3,2,30},{852,2,1,36}};
 //		double[][] Y = {{460},{232},{315},{178}};
 		double[][] X = {{2,3,1},{4,2,3},{7,1,-1}};
 		double[][] Y = {{4},{17},{1}};
-		
-		double[][] para = NE(X, Y);
-		
-		
-		for(int i = 0; i < para.length; i++) {
-			for(int j = 0; j < para[0].length; j++)
-				System.out.print(para[i][j]+" ");
-			System.out.println();
+		try {
+			if(X.length != X[0].length || X.length != Y.length) {
+				System.out.println("参数个数与方程个数不匹配");
+				return ;
+			}
+			double[][] para = NE(X, Y);
+			
+			
+			for(int i = 0; i < para.length; i++) {
+				for(int j = 0; j < para[0].length; j++)
+					System.out.print(para[i][j]+" ");
+				System.out.println();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
 		}
+		
 	}
 }
